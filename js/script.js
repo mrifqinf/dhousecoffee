e10c70e6cf5ca82078712671e5ed7562c4bb3dc5
Muhammad Rifqi Nf <114318041+mrifqinf@users.noreply.github.com>
2023-10-25 14:31:15 +0700

    UI Update

--- js/script.js
+++ js/script.js
@@ -12,6 +12,12 @@ document.querySelector('#search-button').onclick = () => {
     searchBox.focus();
 }
 
+// JS FOR SHOPPING CART CLICK FOR OPEN CART
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart').onclick = () => {
+    shoppingCart.classList.toggle('active');
+}
+
 // JS FOR HMENU FOR SIDEBAR CLICK ANYWHERE
 const hmenus = document.querySelector('#hmenu');
 document.addEventListener('click', function (e) {
@@ -20,10 +26,37 @@ document.addEventListener('click', function (e) {
     }
 })
 
-// JS FOR SEARCH FORM FOR  CLICK ANYWHERE
+// JS FOR SEARCH FORM FOR CLICK ANYWHERE
 const searchbt = document.querySelector('#search-button');
 document.addEventListener('click', function (e) {
     if (!searchbt.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
 })
+
+// JS FOR SHOPPING CART FOR CLICK ANYWHERE
+const shopCart = document.querySelector('#shopping-cart');
+document.addEventListener('click', function (e) {
+    if (!shopCart.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
+})
+
+// JS FOR MODAL DETAIL FOR CLICK ANYWHERE
+const modalDetail = document.querySelector('#detail-modal');
+const buttonDetails = document.querySelectorAll('.detail-modal');
+window.onclick = (e) => {
+    if (e.target === modalDetail) {
+        modalDetail.style.display = 'none';
+    }
+}
+// JS FOR MODAL ITEM CLICK FOR OPEN MODAL DETAIL
+buttonDetails.forEach((btn) => {
+    btn.onclick = (e) => {
+        modalDetail.style.display = 'flex';
+    }
+})
+// JS FOR MODAL ITEM CLICK FOR CLOSE MODAL DETAIL
+document.querySelector('.close-button').onclick = (e) => {
+    modalDetail.style.display = 'none';
+}
